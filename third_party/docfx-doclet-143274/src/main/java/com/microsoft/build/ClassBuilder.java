a0a6aef6c6b77f02b8084263b13779e0b6855a31
Emily Ball <emilyball@google.com>
2021-12-15 17:37:13 -0800

    chore: refactor YmlFilesBuilder
    Break up YmlFilesBuilder into multiple builders to make it easier to follow code and make changes. This change doesn't change any code code, it just moves code into new files.

---
+++ third_party/docfx-doclet-143274/src/main/java/com/microsoft/build/ClassBuilder.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.microsoft.build;
+
+import com.microsoft.lookup.ClassItemsLookup;
+import com.microsoft.lookup.ClassLookup;
+import com.microsoft.model.MetadataFile;
+import com.microsoft.model.MetadataFileItem;
+import com.microsoft.model.Status;
+import com.microsoft.model.TocItem;
+import com.microsoft.model.TocTypeMap;
+import com.microsoft.util.ElementUtil;
+import com.microsoft.util.Utils;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.ElementFilter;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static com.microsoft.build.BuilderUtil.LANGS;
+import static com.microsoft.build.BuilderUtil.getDeprecatedSummary;
+import static com.microsoft.build.BuilderUtil.populateItemFields;
+
+class ClassBuilder {
+    private ElementUtil elementUtil;
+    private ClassLookup classLookup;
+    private ClassItemsLookup classItemsLookup;
+    private String outputPath;
+    private ReferenceBuilder referenceBuilder;
+
+    ClassBuilder(ElementUtil elementUtil, ClassLookup classLookup, ClassItemsLookup classItemsLookup, String outputPath, ReferenceBuilder referenceBuilder) {
+        this.elementUtil = elementUtil;
+        this.classLookup = classLookup;
+        this.classItemsLookup = classItemsLookup;
+        this.outputPath = outputPath;
+        this.referenceBuilder = referenceBuilder;
+    }
+
+    void buildFilesForInnerClasses(Element element, TocTypeMap tocTypeMap, List<MetadataFile> container) {
+        for (TypeElement classElement : elementUtil.extractSortedElements(element)) {
+            String uid = classLookup.extractUid(classElement);
+            String name = classLookup.extractTocName(classElement);
+            String status = classLookup.extractStatus(classElement);
+
+            if (tocTypeMap.get(classElement.getKind().name()) != null) {
+                if (classElement.getKind().name().equals(ElementKind.CLASS.name()) && name.contains("Exception")) {
+                    tocTypeMap.get("EXCEPTION").add(new TocItem(uid, name, status));
+                } else {
+                    tocTypeMap.get(classElement.getKind().name()).add(new TocItem(uid, name, status));
+                }
+            } else {
+                tocTypeMap.get(ElementKind.CLASS.name()).add(new TocItem(uid, name, status));
+            }
+
+            container.add(buildClassYmlFile(classElement));
+            buildFilesForInnerClasses(classElement, tocTypeMap, container);
+        }
+    }
+
+    private MetadataFile buildClassYmlFile(TypeElement classElement) {
+        String fileName = classLookup.extractHref(classElement);
+        MetadataFile classMetadataFile = new MetadataFile(outputPath, fileName);
+        addClassInfo(classElement, classMetadataFile);
+        addConstructorsInfo(classElement, classMetadataFile);
+        addMethodsInfo(classElement, classMetadataFile);
+        addFieldsInfo(classElement, classMetadataFile);
+        referenceBuilder.addReferencesInfo(classElement, classMetadataFile);
+        applyPostProcessing(classMetadataFile);
+        return classMetadataFile;
+    }
+
+    private void addClassInfo(TypeElement classElement, MetadataFile classMetadataFile) {
+        MetadataFileItem classItem = new MetadataFileItem(LANGS, classLookup.extractUid(classElement));
+        classItem.setId(classLookup.extractId(classElement));
+        classItem.setParent(classLookup.extractParent(classElement));
+        addChildren(classElement, classItem.getChildren());
+        populateItemFields(classItem, classLookup, classElement);
+        classItem.setPackageName(classLookup.extractPackageName(classElement));
+        classItem.setTypeParameters(classLookup.extractTypeParameters(classElement));
+        classItem.setInheritance(classLookup.extractSuperclass(classElement));
+        classItem.setInterfaces(classLookup.extractInterfaces(classElement));
+        classItem.setInheritedMethods(classLookup.extractInheritedMethods(classElement));
+        String depMsg = classLookup.extractDeprecatedDescription(classElement);
+        if (depMsg != null) {
+            classItem.setSummary(getDeprecatedSummary(depMsg, classItem.getSummary()));
+            classItem.setStatus(Status.DEPRECATED.toString());
+        }
+        classMetadataFile.getItems().add(classItem);
+    }
+
+    void addConstructorsInfo(TypeElement classElement, MetadataFile classMetadataFile) {
+        for (ExecutableElement constructorElement : ElementFilter.constructorsIn(classElement.getEnclosedElements())) {
+            MetadataFileItem constructorItem = buildMetadataFileItem(constructorElement);
+            constructorItem.setOverload(classItemsLookup.extractOverload(constructorElement));
+            constructorItem.setContent(classItemsLookup.extractConstructorContent(constructorElement));
+            constructorItem.setParameters(classItemsLookup.extractParameters(constructorElement));
+            String depMsg = classItemsLookup.extractDeprecatedDescription(constructorElement);
+            if (depMsg != null) {
+                constructorItem.setSummary(getDeprecatedSummary(depMsg, constructorItem.getSummary()));
+                constructorItem.setStatus(Status.DEPRECATED.toString());
+            }
+            classMetadataFile.getItems().add(constructorItem);
+
+            referenceBuilder.addParameterReferences(constructorItem, classMetadataFile);
+            referenceBuilder.addOverloadReferences(constructorItem, classMetadataFile);
+        }
+    }
+
+    private void addMethodsInfo(TypeElement classElement, MetadataFile classMetadataFile) {
+        ElementFilter.methodsIn(classElement.getEnclosedElements()).stream()
+                .filter(methodElement -> !Utils.isPrivateOrPackagePrivate(methodElement))
+                .forEach(methodElement -> {
+                    MetadataFileItem methodItem = buildMetadataFileItem(methodElement);
+                    methodItem.setOverload(classItemsLookup.extractOverload(methodElement));
+                    methodItem.setContent(classItemsLookup.extractMethodContent(methodElement));
+                    methodItem.setExceptions(classItemsLookup.extractExceptions(methodElement));
+                    methodItem.setParameters(classItemsLookup.extractParameters(methodElement));
+                    methodItem.setReturn(classItemsLookup.extractReturn(methodElement));
+                    methodItem.setOverridden(classItemsLookup.extractOverridden(methodElement));
+                    String depMsg = classItemsLookup.extractDeprecatedDescription(methodElement);
+                    if (depMsg != null) {
+                        methodItem.setSummary(getDeprecatedSummary(depMsg, methodItem.getSummary()));
+                        methodItem.setStatus(Status.DEPRECATED.toString());
+                    }
+
+                    classMetadataFile.getItems().add(methodItem);
+                    referenceBuilder.addExceptionReferences(methodItem, classMetadataFile);
+                    referenceBuilder.addParameterReferences(methodItem, classMetadataFile);
+                    referenceBuilder.addReturnReferences(methodItem, classMetadataFile);
+                    referenceBuilder.addOverloadReferences(methodItem, classMetadataFile);
+                });
+    }
+
+    private void addFieldsInfo(TypeElement classElement, MetadataFile classMetadataFile) {
+        ElementFilter.fieldsIn(classElement.getEnclosedElements()).stream()
+                .filter(fieldElement -> !Utils.isPrivateOrPackagePrivate(fieldElement))
+                .forEach(fieldElement -> {
+                    MetadataFileItem fieldItem = buildMetadataFileItem(fieldElement);
+                    fieldItem.setContent(classItemsLookup.extractFieldContent(fieldElement));
+                    fieldItem.setReturn(classItemsLookup.extractReturn(fieldElement));
+                    String depMsg = classItemsLookup.extractDeprecatedDescription(fieldElement);
+                    if (depMsg != null) {
+                        fieldItem.setSummary(getDeprecatedSummary(depMsg, fieldItem.getSummary()));
+                        fieldItem.setStatus(Status.DEPRECATED.toString());
+                    }
+
+                    classMetadataFile.getItems().add(fieldItem);
+                    referenceBuilder.addReturnReferences(fieldItem, classMetadataFile);
+                });
+    }
+
+    private void applyPostProcessing(MetadataFile classMetadataFile) {
+        referenceBuilder.expandComplexGenericsInReferences(classMetadataFile);
+    }
+
+    private MetadataFileItem buildMetadataFileItem(Element element) {
+        return new MetadataFileItem(LANGS, classItemsLookup.extractUid(element)) {{
+            String name = classItemsLookup.extractName(element);
+            setId(classItemsLookup.extractId(element));
+            setParent(classItemsLookup.extractParent(element));
+            setName(name);
+            setNameWithType(classItemsLookup.extractNameWithType(element));
+            setFullName(classItemsLookup.extractFullName(element));
+            setType(classItemsLookup.extractType(element));
+            setJavaType(classItemsLookup.extractJavaType(element));
+            setPackageName(classItemsLookup.extractPackageName(element));
+            setSummary(classItemsLookup.extractSummary(element));
+        }};
+    }
+
+    private void addChildren(TypeElement classElement, List<String> children) {
+        collect(classElement, children, ElementFilter::constructorsIn, classItemsLookup::extractUid);
+        collect(classElement, children, ElementFilter::methodsIn, classItemsLookup::extractUid);
+        collect(classElement, children, ElementFilter::fieldsIn, classItemsLookup::extractUid);
+        collect(classElement, children, ElementFilter::typesIn, String::valueOf);
+    }
+
+    private void collect(TypeElement classElement, List<String> children,
+                 Function<Iterable<? extends Element>, List<? extends Element>> selectFunc,
+                 Function<? super Element, String> mapFunc) {
+
+        List<? extends Element> elements = selectFunc.apply(classElement.getEnclosedElements());
+        children.addAll(filterPrivateElements(elements).stream()
+                .map(mapFunc).collect(Collectors.toList()));
+    }
+
+    private List<? extends Element> filterPrivateElements(List<? extends Element> elements) {
+        return elements.stream()
+                .filter(element -> !Utils.isPrivateOrPackagePrivate(element)).collect(Collectors.toList());
+    }
+}
---
+++ third_party/docfx-doclet-143274/src/main/java/com/microsoft/build/ReferenceBuilder.java
@@ -0,0 +1,283 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.microsoft.build;
+
+import com.microsoft.lookup.ClassLookup;
+import com.microsoft.model.MetadataFile;
+import com.microsoft.model.MetadataFileItem;
+import com.microsoft.model.SpecViewModel;
+import com.microsoft.util.ElementUtil;
+import jdk.javadoc.doclet.DocletEnvironment;
+import org.apache.commons.lang3.RegExUtils;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.ElementFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.microsoft.build.BuilderUtil.getJavaSpec;
+import static com.microsoft.build.BuilderUtil.populateItemFields;
+import static com.microsoft.build.BuilderUtil.replaceUidAndSplit;
+import static com.microsoft.build.BuilderUtil.splitUidWithGenericsIntoClassNames;
+
+class ReferenceBuilder {
+    private final Pattern JAVA_PATTERN = Pattern.compile("^java.*");
+    private final Pattern PROTOBUF_PATTERN = Pattern.compile("^com.google.protobuf.*");
+    private final Pattern GAX_PATTERN = Pattern.compile("^com.google.api.gax.*");
+    private final Pattern APICOMMON_PATTERN = Pattern.compile("^com.google.api.core.*");
+    private final Pattern LONGRUNNING_PATTERN = Pattern.compile("^com.google.longrunning.*");
+    private final Pattern ENDING_PATTERN = Pattern.compile(".*<\\?>$");
+    private final String PRIMITIVE_URL = "https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html";
+    private final String JAVA_BASE_URL = "https://docs.oracle.com/javase/8/docs/api/";
+    private final DocletEnvironment environment;
+    private final ClassLookup classLookup;
+    private final ElementUtil elementUtil;
+
+    ReferenceBuilder(DocletEnvironment environment, ClassLookup classLookup, ElementUtil elementUtil) {
+        this.environment = environment;
+        this.classLookup = classLookup;
+        this.elementUtil = elementUtil;
+    }
+
+    MetadataFileItem buildClassReference(TypeElement classElement) {
+        MetadataFileItem referenceItem = new MetadataFileItem(classLookup.extractUid(classElement));
+        referenceItem.setName(classLookup.extractName(classElement));
+        referenceItem.setNameWithType(classLookup.extractNameWithType(classElement));
+        referenceItem.setFullName(classLookup.extractFullName(classElement));
+        return referenceItem;
+    }
+
+    void addReferencesInfo(TypeElement classElement, MetadataFile classMetadataFile) {
+        MetadataFileItem classReference = new MetadataFileItem(classLookup.extractUid(classElement));
+        classReference.setParent(classLookup.extractParent(classElement));
+        populateItemFields(classReference, classLookup, classElement);
+        classReference.setTypeParameters(classLookup.extractTypeParameters(classElement));
+
+        addTypeParameterReferences(classReference, classMetadataFile);
+        addSuperclassAndInterfacesReferences(classElement, classMetadataFile);
+        addInnerClassesReferences(classElement, classMetadataFile);
+    }
+
+    void addChildrenReferences(Element element, List<String> packageChildren,
+                               Set<MetadataFileItem> referencesCollector) {
+        for (TypeElement classElement : elementUtil.extractSortedElements(element)) {
+            referencesCollector.add(buildClassReference(classElement));
+
+            packageChildren.add(classLookup.extractUid(classElement));
+            addChildrenReferences(classElement, packageChildren, referencesCollector);
+        }
+    }
+
+    String getJavaReferenceHref(String uid) {
+        if (uid == null || uid.equals("")) {
+            return JAVA_BASE_URL;
+        }
+        //  example1 uid: "java.lang.Object.equals(java.lang.Object)"
+        //  example2 uid: "java.lang.Object"
+        String endURL = uid.replaceAll("<T>", "");
+
+        Pattern argPattern = Pattern.compile(".*\\(.*\\).*");
+        if (argPattern.matcher(endURL).find()) {
+            // example1
+            // argumentSplit: ["java.lang.Object.equals", "java.lang.Object)"]
+            // nameSplit: ["java", "lang", "Object", "equals"]
+            List<String> argumentSplit = Arrays.asList(endURL.split("\\("));
+            List<String> nameSplit = Arrays.asList(argumentSplit.get(0).split("\\."));
+
+            // className: "java/lang/Object"
+            // methodName: "#equals"
+            // argumentsName: "#java.lang.Object-"
+            String className = String.join("/", nameSplit.subList(0, nameSplit.size() - 1));
+            String methodName = "#" + nameSplit.get(nameSplit.size() - 1);
+            String argumentsName = argumentSplit.get(1).replaceAll("[,)]", "-");
+
+            // endURL: "java/lang/Object.html#equals-java.lang.Object-"
+            endURL = className + ".html" + methodName + "-" + argumentsName;
+        } else {
+            // example2
+            // endURL = java/lang/Object.html
+            endURL = endURL.replaceAll("\\.", "/");
+            endURL = endURL + ".html";
+        }
+        return JAVA_BASE_URL + endURL;
+    }
+
+    void updateExternalReferences(List<MetadataFile> classMetadataFiles) {
+        classMetadataFiles.forEach(file -> file.getReferences()
+                .forEach(ref -> updateExternalReference(ref)));
+    }
+
+    private void updateExternalReference(MetadataFileItem reference) {
+        String uid = reference.getUid();
+        uid = updateReferenceUid(uid);
+
+        if (isJavaPrimitive(uid)) {
+            reference.setHref(PRIMITIVE_URL);
+            return;
+        }
+        if (isJavaLibrary(uid)) {
+            reference.setHref(getJavaReferenceHref(uid));
+        }
+        if (isExternalReference(uid)) {
+            reference.setIsExternal(true);
+        }
+        if (reference.getSpecForJava().size() > 0) {
+            for (SpecViewModel spec : reference.getSpecForJava()) {
+                String specUid = spec.getUid();
+                if (specUid != null) {
+                    if (isJavaPrimitive(specUid)) {
+                        spec.setHref(PRIMITIVE_URL);
+                    }
+                    if (isJavaLibrary(specUid)) {
+                        spec.setHref(getJavaReferenceHref(specUid));
+                    }
+                    if (isExternalReference(specUid)) {
+                        spec.setIsExternal(true);
+                    }
+                }
+            }
+        }
+    }
+
+    private String updateReferenceUid(String uid) {
+        if (ENDING_PATTERN.matcher(uid).find()) {
+            uid = uid.replace("<?>", "");
+        }
+        return uid;
+    }
+
+    private boolean isExternalReference(String uid) {
+        return (PROTOBUF_PATTERN.matcher(uid).find() || GAX_PATTERN.matcher(uid).find() || APICOMMON_PATTERN.matcher(uid).find() || GAX_PATTERN.matcher(uid).find() || LONGRUNNING_PATTERN.matcher(uid).find());
+    }
+
+    private boolean isJavaPrimitive(String uid) {
+        return (uid.equals("boolean") || uid.equals("int") || uid.equals("byte") || uid.equals("long") || uid.equals("float") || uid.equals("double") || uid.equals("char") || uid.equals("short"));
+    }
+
+    private boolean isJavaLibrary(String uid) {
+        return JAVA_PATTERN.matcher(uid).find();
+    }
+
+    void addParameterReferences(MetadataFileItem methodItem, MetadataFile classMetadataFile) {
+        classMetadataFile.getReferences().addAll(
+                methodItem.getSyntax().getParameters().stream()
+                        .map(parameter -> buildRefItem(parameter.getType()))
+                        .filter(o -> !classMetadataFile.getItems().contains(o))
+                        .collect(Collectors.toList()));
+    }
+
+    void addReturnReferences(MetadataFileItem methodItem, MetadataFile classMetadataFile) {
+        classMetadataFile.getReferences().addAll(
+                Stream.of(methodItem.getSyntax().getReturnValue())
+                        .filter(Objects::nonNull)
+                        .map(returnValue -> buildRefItem(returnValue.getReturnType()))
+                        .filter(o -> !classMetadataFile.getItems().contains(o))
+                        .collect(Collectors.toList()));
+    }
+
+    void addExceptionReferences(MetadataFileItem methodItem, MetadataFile classMetadataFile) {
+        classMetadataFile.getReferences().addAll(
+                methodItem.getExceptions().stream()
+                        .map(exceptionItem -> buildRefItem(exceptionItem.getType()))
+                        .filter(o -> !classMetadataFile.getItems().contains(o))
+                        .collect(Collectors.toList()));
+    }
+
+    void addTypeParameterReferences(MetadataFileItem methodItem, MetadataFile classMetadataFile) {
+        classMetadataFile.getReferences().addAll(
+                methodItem.getSyntax().getTypeParameters().stream()
+                        .map(typeParameter -> {
+                            String id = typeParameter.getId();
+                            return new MetadataFileItem(id, id, false);
+                        }).collect(Collectors.toList()));
+    }
+
+    void addSuperclassAndInterfacesReferences(TypeElement classElement, MetadataFile classMetadataFile) {
+        classMetadataFile.getReferences().addAll(classLookup.extractReferences(classElement));
+    }
+
+    void addInnerClassesReferences(TypeElement classElement, MetadataFile classMetadataFile) {
+        classMetadataFile.getReferences().addAll(
+                ElementFilter.typesIn(elementUtil.extractSortedElements(classElement)).stream()
+                        .map(this::buildClassReference)
+                        .collect(Collectors.toList()));
+    }
+
+    void addOverloadReferences(MetadataFileItem item, MetadataFile classMetadataFile) {
+        classMetadataFile.getReferences().add(new MetadataFileItem(item.getOverload()) {{
+            setName(RegExUtils.removeAll(item.getName(), "\\(.*\\)$"));
+            setNameWithType(RegExUtils.removeAll(item.getNameWithType(), "\\(.*\\)$"));
+            setFullName(RegExUtils.removeAll(item.getFullName(), "\\(.*\\)$"));
+            setPackageName(item.getPackageName());
+        }});
+    }
+
+    /**
+     * Replace one record in 'references' with several records in this way:
+     * <pre>
+     * a.b.c.List<df.mn.ClassOne<tr.T>> ->
+     *     - a.b.c.List
+     *     - df.mn.ClassOne
+     *     - tr.T
+     * </pre>
+     */
+    void expandComplexGenericsInReferences(MetadataFile classMetadataFile) {
+        Set<MetadataFileItem> additionalItems = new LinkedHashSet<>();
+        Iterator<MetadataFileItem> iterator = classMetadataFile.getReferences().iterator();
+        while (iterator.hasNext()) {
+            MetadataFileItem item = iterator.next();
+            String uid = item.getUid();
+            if (!uid.endsWith("*") && uid.contains("<")) {
+                List<String> classNames = splitUidWithGenericsIntoClassNames(uid);
+                additionalItems.addAll(classNames.stream()
+                        .map(s -> new MetadataFileItem(s, classLookup.makeTypeShort(s), false))
+                        .collect(Collectors.toSet()));
+            }
+        }
+        // Remove items which already exist in 'items' section (compared by 'uid' field)
+        additionalItems.removeAll(classMetadataFile.getItems());
+
+        classMetadataFile.getReferences().addAll(additionalItems);
+    }
+
+    MetadataFileItem buildRefItem(String uid) {
+        if (!uid.endsWith("*") && (uid.contains("<") || uid.contains("[]"))) {
+            return new MetadataFileItem(uid, getJavaSpec(replaceUidAndSplit(uid)));
+        } else {
+            List<String> fullNameList = new ArrayList<>();
+
+            environment.getIncludedElements().forEach(
+                    element -> elementUtil.extractSortedElements(element).forEach(
+                            typeElement -> fullNameList.add(classLookup.extractFullName(typeElement)))
+            );
+
+            if (fullNameList.contains(uid)) {
+                return new MetadataFileItem(uid, classLookup.makeTypeShort(uid), false);
+            } else {
+                return new MetadataFileItem(uid, getJavaSpec(replaceUidAndSplit(uid)));
+            }
+        }
+    }
+}
