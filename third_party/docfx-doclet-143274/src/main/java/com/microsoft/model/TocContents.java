72dc16349a22de088b2360823b97676729d430b7
Emily Ball <emilyball@google.com>
2022-04-13 16:25:19 -0700

    feat: disableChangelog option (#119)

--- third_party/docfx-doclet-143274/src/main/java/com/microsoft/model/TocContents.java
+++ third_party/docfx-doclet-143274/src/main/java/com/microsoft/model/TocContents.java
@@ -23,22 +23,24 @@ public class TocContents {
     private final String projectName;
     private final List<Object> contents = new ArrayList<>();
 
-    public TocContents(String projectName, List<TocItem> items) {
+    public TocContents(String projectName, boolean disableChangelog, List<TocItem> items) {
         this.projectName = projectName;
 
         if (projectName == null || projectName.equals("")) {
             contents.addAll(items);
         } else {
             //  only include product hierarchy and guides if projectName included
-            createTocContents(projectName, items);
+            createTocContents(projectName, disableChangelog, items);
         }
     }
 
-    private void createTocContents(String projectName, List<TocItem> items) {
+    private void createTocContents(String projectName, boolean disableChangelog, List<TocItem> items) {
         List<Object> tocItems = new ArrayList<>();
         // combine guides and tocItems
         tocItems.add(new Guide("Overview", "overview.html"));
-        tocItems.add(new Guide("Version history", "history.md"));
+        if (!disableChangelog) {
+            tocItems.add(new Guide("Version history", "history.md"));
+        }
         tocItems.addAll(items);
         // wrap guides + tocItems with product hierarchy
         contents.add(new ProjectContents(projectName, tocItems));
