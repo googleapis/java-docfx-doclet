672f8ff9038e395e76edbb638e3d48a2d06e9f59
Burke Davison <burkedavison@google.com>
2023-10-02 11:31:49 -0400

    chore: refactor ApiVersionPackageToc for better testing

--- third_party/docfx-doclet-143274/src/main/java/com/microsoft/model/ApiVersionPackageToc.java
+++ third_party/docfx-doclet-143274/src/main/java/com/microsoft/model/ApiVersionPackageToc.java
@@ -2,6 +2,7 @@ package com.microsoft.model;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class ApiVersionPackageToc {
@@ -18,109 +19,89 @@ public class ApiVersionPackageToc {
   static final String RESOURCE_NAMES = "Resource names";
   static final String UNCATEGORIZED = "Other";
 
-  private final List<TocItem> clients = new ArrayList<>();
-  private final List<TocItem> requestsAndResponses = new ArrayList<>();
-  private final List<TocItem> settings = new ArrayList<>();
-  private final List<TocItem> builders = new ArrayList<>();
-  private final List<TocItem> enums = new ArrayList<>();
-  private final List<TocItem> exceptions = new ArrayList<>();
-  private final List<TocItem> interfaces = new ArrayList<>();
-  private final List<TocItem> messages = new ArrayList<>();
-  private final List<TocItem> paging = new ArrayList<>();
-  private final List<TocItem> resourceNames = new ArrayList<>();
-  private final List<TocItem> uncategorized = new ArrayList<>();
+  private final LinkedHashMap<String, List<TocItem>> visibleCategories = new LinkedHashMap<>();
+  private final LinkedHashMap<String, List<TocItem>> hiddenCategories = new LinkedHashMap<>();
+  
+  public ApiVersionPackageToc() {
+    // Order here determines final organization order.
+    visibleCategories.put(CLIENTS, new ArrayList<>());
+    visibleCategories.put(REQUESTS_AND_RESPONSES, new ArrayList<>());
+    visibleCategories.put(SETTINGS, new ArrayList<>());
 
+    hiddenCategories.put(BUILDERS, new ArrayList<>());
+    hiddenCategories.put(ENUMS, new ArrayList<>());
+    hiddenCategories.put(EXCEPTIONS, new ArrayList<>());
+    hiddenCategories.put(MESSAGES, new ArrayList<>());
+    hiddenCategories.put(PAGING, new ArrayList<>());
+    hiddenCategories.put(RESOURCE_NAMES, new ArrayList<>());
+    hiddenCategories.put(INTERFACES, new ArrayList<>());
+    hiddenCategories.put(UNCATEGORIZED, new ArrayList<>());
+  }
+  
   public void addClient(TocItem tocItem) {
-    clients.add(tocItem);
+    visibleCategories.get(CLIENTS).add(tocItem);
   }
 
   public void addRequestOrResponse(TocItem tocItem) {
-    requestsAndResponses.add(tocItem);
+    visibleCategories.get(REQUESTS_AND_RESPONSES).add(tocItem);
   }
 
   public void addSettings(TocItem tocItem) {
-    settings.add(tocItem);
+    visibleCategories.get(SETTINGS).add(tocItem);
   }
 
   public void addBuilder(TocItem tocItem) {
-    builders.add(tocItem);
+    hiddenCategories.get(BUILDERS).add(tocItem);
   }
 
   public void addEnum(TocItem tocItem) {
-    enums.add(tocItem);
+    hiddenCategories.get(ENUMS).add(tocItem);
   }
 
   public void addException(TocItem tocItem) {
-    exceptions.add(tocItem);
+    hiddenCategories.get(EXCEPTIONS).add(tocItem);
   }
 
   public void addInterface(TocItem tocItem) {
-    interfaces.add(tocItem);
+    hiddenCategories.get(INTERFACES).add(tocItem);
   }
 
   public void addMessage(TocItem tocItem) {
-    messages.add(tocItem);
+    hiddenCategories.get(MESSAGES).add(tocItem);
   }
 
   public void addUncategorized(TocItem tocItem) {
-    uncategorized.add(tocItem);
+    hiddenCategories.get(UNCATEGORIZED).add(tocItem);
   }
 
   public void addPaging(TocItem tocItem) {
-    paging.add(tocItem);
+    hiddenCategories.get(PAGING).add(tocItem);
   }
 
   public void addResourceName(TocItem tocItem) {
-    resourceNames.add(tocItem);
+    hiddenCategories.get(RESOURCE_NAMES).add(tocItem);
   }
 
   /** Build a list of TocItems for inclusion in the library's table of contents */
   public List<TocItem> toList() {
     List<TocItem> toc = new ArrayList<>();
-    if (!clients.isEmpty()) {
-      toc.add(createCategory(CLIENTS, clients));
-    }
-    if (!requestsAndResponses.isEmpty()) {
-      toc.add(createCategory(REQUESTS_AND_RESPONSES, requestsAndResponses));
-    }
-    if (!settings.isEmpty()) {
-      toc.add(createCategory(SETTINGS, settings));
-    }
-    if (!builders.isEmpty()
-        || !enums.isEmpty()
-        || !exceptions.isEmpty()
-        || !interfaces.isEmpty()
-        || !messages.isEmpty()
-        || !paging.isEmpty()
-        || !resourceNames.isEmpty()
-        || !uncategorized.isEmpty()) {
-      TocItem allOthers = new TocItem(ALL_OTHERS, ALL_OTHERS, null);
-      if (!builders.isEmpty()) {
-        allOthers.getItems().add(createCategory(BUILDERS, builders));
+    
+    visibleCategories.forEach((name, category) -> {
+      if (! category.isEmpty()) {
+        toc.add(createCategory(name, category));
       }
-      if (!enums.isEmpty()) {
-        allOthers.getItems().add(createCategory(ENUMS, enums));
+    });
+
+    TocItem allOthers = new TocItem(ALL_OTHERS, ALL_OTHERS, null);
+    hiddenCategories.forEach((name, category) -> {
+      if (! category.isEmpty()) {
+        allOthers.getItems().add(createCategory(name, category));
       }
-      if (!exceptions.isEmpty()) {
-        allOthers.getItems().add(createCategory(EXCEPTIONS, exceptions));
-      }
-      if (!messages.isEmpty()) {
-        allOthers.getItems().add(createCategory(MESSAGES, messages));
-      }
-      if (!paging.isEmpty()) {
-        allOthers.getItems().add(createCategory(PAGING, paging));
-      }
-      if (!resourceNames.isEmpty()) {
-        allOthers.getItems().add(createCategory(RESOURCE_NAMES, resourceNames));
-      }
-      if (!interfaces.isEmpty()) {
-        allOthers.getItems().add(createCategory(INTERFACES, interfaces));
-      }
-      if (!uncategorized.isEmpty()) {
-        allOthers.getItems().add(createCategory(UNCATEGORIZED, uncategorized));
-      }
+    });
+    if (allOthers.getItems().size() > 0) {
       toc.add(allOthers);
     }
+
     return toc;
   }
 
